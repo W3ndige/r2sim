9c6d065e54c900b9fd866aef2d3b7cbf211cb0e1
W3ndige <w3ndige@gmail.com>
2021-04-27 17:04:25 +0000

    Add diffing between similar functions (#2)

--- r2sim/utils.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Dict
-
-
-def get_opcodes_function_data(function_data: List[Dict[str, str]]) -> List[str]:
-    return list(map(lambda x: x["opcode"], function_data))
-
-
-def get_opcodes_types_function_data(function_data: List[Dict[str, str]]) -> List[str]:
-    return list(map(lambda x: x["type"], function_data))
